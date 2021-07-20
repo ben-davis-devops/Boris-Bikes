1c627d79a3cf0072c577b88339a50f2b339428a4
Ben Davis <ben.david.davis16@gmail.com>
2021-07-20 15:53:05 +0100

    CH 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,27 @@
 require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+
+  it { is_expected.to respond_to(:dock).with(1).argument}
   
   it 'releases working bikes' do
     bike = subject.release_bike # bike = new element(method - release bike)
     expect(bike).to be_working # checking if be_working function shows that the bike is working
   end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  # it 'a message describing the test' do
+  #  expect(the method).to eq the argument output
+  # end
 end
 
